f12c9fb98d5d06b44ab36443b242ba8367290864
Inamul <inamulhoque097@gmail.com>
2022-06-21 20:24:46 +0530

    Added: Test first name is valid or not.

--- src/com/bridgelabz/junit/UserRegistrationTest.java
+++ src/com/bridgelabz/junit/UserRegistrationTest.java
@@ -3,12 +3,9 @@ package com.bridgelabz.junit;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.Scanner;
-
 public class UserRegistrationTest {
     @Test
-    public void testFirstNameHappy() {
-        Scanner obj = new Scanner(System.in);
+    public void testFirstName() {
         UserRegistration userRegistration = new UserRegistration();
         boolean isValid = userRegistration.validFirstName("Inam");
         Assert.assertEquals(true, isValid);
