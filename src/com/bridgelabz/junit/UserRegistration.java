21889be3593a8a3f00280158dd44058fa138ae88
Inamul <inamulhoque097@gmail.com>
2022-06-21 20:28:09 +0530

    Added: Test first name is valid or not

--- src/com/bridgelabz/junit/UserRegistration.java
+++ src/com/bridgelabz/junit/UserRegistration.java
@@ -3,7 +3,7 @@ package com.bridgelabz.junit;
 public class UserRegistration {
     public boolean validFirstName(String name) {
 
-        String nameRegex = "^[A-Z]{1}[a-z]{2,}$";
+        String nameRegex = "^[A-Z][a-z]{2,}";
         return name.matches(nameRegex);
     }
 }
